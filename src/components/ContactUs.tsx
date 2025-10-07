03cd643bcd8261d9521f6b648aa5322755cdc7c6
hamza-mqbl <hamzaaliics@gmail.com>
2025-10-07 14:08:46 +0500

    style: Correct padding class in ContactUs component for consistent layout

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -2,7 +2,7 @@ import { ContactForm } from "./ContactForm";
 
 export function ContactUs() {
   return (
-    <section className="w-full pt-[105px] mb-4">
+    <section className="w-full py-[105px] mb-4">
       <div className="w-full px-4 sm:px-6 md:px-8 lg:px-16 xl:px-24 2xl:px-[106px]">
         <div className="max-w-[1440px] mx-auto">
           <div className="mt-8  flex flex-col 2xl:gap-[60px] xl:gap-16 lg:gap-12 md:gap-10 sm:gap-8 gap-6 ">
